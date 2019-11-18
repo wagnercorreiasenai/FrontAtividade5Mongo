f6cc2998f9e023bcd0b4af63e373205d4fd5fec4
wagner <wagner@wagner-aspire>
2019-11-18 08:20:18 -0300

    Iniciado a página de novo post

--- public_html/js/novo-post.js
+++ public_html/js/novo-post.js
@@ -11,6 +11,40 @@ function setarDataPublicacao() {
 
 }
 
+var contTopico = 1;
+function criarTopicoPrincipal() {
+
+    contTopico++;
+
+    conteudoTopico = '<div class="form-group">\n\
+                      <input autofocus type="text" class="form-control" id="t' + contTopico + '" name="t' + contTopico + '" placeholder="Informe o título da sessão" />\n\
+                      </div>\n\
+                      <div class="form-group">\n\
+                      <textarea placeholder="Conteúdo da sessão" id="c' + contTopico + '" name="c' + contTopico + '" class="form-control"></textarea>\n\
+                      </div>\n\
+                      <button class="btn btn-default" onclick="criarSubtopico(\'top' + contTopico + '\');">Adicionar sub-tópico</button>\n\
+                      <hr/>';
+
+    conteudoTopico = '<li id="top' + contTopico + '">' + conteudoTopico + '</li>';
+
+    $('#sessoes').append(conteudoTopico);
+
+}
+
+function criarSubtopico(codses) {
+
+    conteudoTopico = '<div class="form-group">\n\
+                      <input autofocus type="text" class="form-control" id="t' + contTopico + '" name="t' + contTopico + '" placeholder="Informe o título da sessão" />\n\
+                      </div>\n\
+                      <div class="form-group">\n\
+                      <textarea placeholder="Conteúdo da sessão" id="c' + contTopico + '" name="c' + contTopico + '" class="form-control"></textarea>\n\
+                      </div>\n\
+                      <button class="btn btn-default" onclick="criarSubtopico(\'top' + contTopico + '\');">Adicionar sub-tópico</button>\n\
+                      <hr/>';
+
+}
+
 window.onload = function () {
     setarDataPublicacao();
+    //criarTopico();
 };
